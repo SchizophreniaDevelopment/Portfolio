8a9f7b3c6e0f4c125b1aebc93984da220ffa1669
bluewarrior777 <77178724+bluewarrior777@users.noreply.github.com>
2022-12-17 22:14:12 -0600

    maybe?

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 function sendEmail() {
-  event.preventDefault(); // prevent the form from submitting
-
   // get the form data
   var form = document.getElementById('contact-form');
   var data = new FormData(form);
+  data.append('to', 'noah.t.scott06@gmail.com');
 
   // send the email using the Fetch API
-  fetch('https://example.com/sendemail', {
+  fetch('https://formsubmit.co/noah.t.scott06@gmail.com', {
     method: 'POST',
     body: data
   })
   .then(response => response.text())
   .then(result => {
     console.log(result);
+    clearForm();
   });
+}
+
+function clearForm() {
+  // reset the form
+  document.getElementById('contact-form').reset();
 }
